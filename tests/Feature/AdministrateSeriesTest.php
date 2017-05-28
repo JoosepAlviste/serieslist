0a66f99ba593432562020cdb23b307414e3d0ef3
Joosep Alviste <joosep.alviste@gmail.com>
2017-05-28 22:10:45 +0300

    Stub not admin create test

--- tests/Feature/AdministrateSeriesTest.php
+++ tests/Feature/AdministrateSeriesTest.php
@@ -12,7 +12,7 @@ class AdministrateSeriesTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function an_admin_user_can_create_a_thread()
+    public function an_admin_user_can_create_a_series()
     {
         $this->signIn(factory(User::class)->create([
             'is_admin' => true,
@@ -22,6 +22,19 @@ class AdministrateSeriesTest extends TestCase
         $this->post('/series', $series->toArray());
 
         $this->assertDatabaseHas('series', $series->toArray());
+    }
+
+    /** @test **/
+    public function a_user_who_is_not_an_admin_may_not_create_a_series()
+    {
+        // Given a user is logged in who is not an admin
+        $this->signIn();
+
+        // If a store request is submitted
+
+        // The user should be redirected to /login
+
+        // There should not be a series in the database
     }
 }
 
