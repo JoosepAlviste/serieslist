bec78a57b723d60899e81a05871dc763dae96d4f
Joosep Alviste <joosep.alviste@gmail.com>
2017-05-28 22:08:25 +0300

    Refactor test

--- tests/Feature/AdministrateSeriesTest.php
+++ tests/Feature/AdministrateSeriesTest.php
@@ -14,18 +14,13 @@ class AdministrateSeriesTest extends TestCase
     /** @test */
     public function an_admin_user_can_create_a_thread()
     {
-        // Given an admin user is logged in
-        $user = factory(User::class)->create([
+        $this->signIn(factory(User::class)->create([
             'is_admin' => true,
-        ]);
+        ]));
 
-        $this->signIn($user);
-
-        // If a request to create a series is received
         $series = factory(Series::class)->make();
         $this->post('/series', $series->toArray());
 
-        // A series should be created with the given parameters
         $this->assertDatabaseHas('series', $series->toArray());
     }
 }
