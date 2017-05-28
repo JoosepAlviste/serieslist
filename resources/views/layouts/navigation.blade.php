80c17a18e6f14b061cd9febe67fa493ef4e072e6
Joosep Alviste <joosep.alviste@gmail.com>
2017-05-28 21:39:37 +0300

    Move User model to Models dir

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -57,7 +57,7 @@
                 </a>
                 <a class="nav-item" href="{{ url('/logout') }}"
                    onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
+                            document.getElementById('logout-form').submit();">
                     Logout
                 </a>
 
