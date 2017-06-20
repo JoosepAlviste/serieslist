645c125bf85a9de75dda6d49a4ed500b257112ac
Joosep Alviste <joosep.alviste@gmail.com>
2017-06-20 03:05:58 +0300

    Try travis to heroku deploy

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Serieslist
+                    Serieslist changed
                 </div>
             </div>
         </div>
