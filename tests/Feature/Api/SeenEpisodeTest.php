3fd031d198dbe73c7cc18168284ef90131fe1e33
Joosep Alviste <joosep.alviste@gmail.com>
2017-08-02 19:07:06 +0300

    Test seen episode toggle api

---
+++ tests/Feature/Api/SeenEpisodeTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Episode;
+use App\Models\SeenEpisode;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class SeenEpisodeTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function when_an_episode_is_marked_as_seen_the_next_episode_will_be_returned()
+    {
+        $episode = create(Episode::class);
+        $episodeTwo = create(
+            Episode::class,
+            ['season_id' => $episode->season_id, 'number' => $episode->number + 1]
+        );
+
+        $response = $this->markEpisodeSeen($episode);
+        $response->assertJson([
+            'title' => $episode->title,
+            'nextEpisode' => [
+                'title' => $episodeTwo->title,
+            ],
+        ]);
+    }
+
+    protected function markEpisodeSeen($episode = null)
+    {
+        if (!auth()->check()) {
+            $this->signIn();
+        }
+
+        $episode = $episode ?: create(Episode::class);
+        $response = $this->json('post', "/episodes/{$episode->id}/seen-episodes");
+
+        return $response;
+    }
+}
+
