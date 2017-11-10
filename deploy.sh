8e0a0a4893badeda5ddcb014d1886baaca14246e
Joosep Alviste <joosep.alviste@gmail.com>
2017-11-10 16:04:34 +0200

    Update deploy script, remove Heroku

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-    ssh -o "StrictHostKeyChecking no" -i deploy-key deploy@serieslist.joosep.xyz 'cd /var/www/html/serieslist && git pull'
-else
-    echo "Not deploying, since this branch isn't master."
-fi
---
+++ deploy/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    read -r -d '' SSH_COMMAND <<< EOM
+    cd /var/www/html/serieslist &&
+    git pull &&
+    composer install &&
+    php artisan migrate --force
+EOM
+    ssh -o "StrictHostKeyChecking no" -i deploy-key deploy@serieslist.joosep.xyz $SSH_COMMAND
+else
+    echo "Not deploying, since this branch isn't master."
+fi
