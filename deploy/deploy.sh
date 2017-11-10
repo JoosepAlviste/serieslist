f2a68b73273864c93ccaad1b60aec06c2d6c119a
Joosep Alviste <joosep.alviste@gmail.com>
2017-11-10 16:29:35 +0200

    Fix typo...

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    read -r -d '' SSH_COMMAND <<< EOM
+    read -r -d '' SSH_COMMAND << EOM
     cd /var/www/html/serieslist &&
     git pull &&
     composer install &&
