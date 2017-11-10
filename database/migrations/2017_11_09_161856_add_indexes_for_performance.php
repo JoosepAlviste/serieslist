1a014b9d2db894c549808297cac0997f8ae6748c
Joosep Alviste <joosep.alviste@gmail.com>
2017-11-10 13:53:01 +0200

    Fix migration

--- database/migrations/2017_11_09_161856_add_indexes_for_performance.php
+++ database/migrations/2017_11_09_161856_add_indexes_for_performance.php
@@ -13,16 +13,6 @@ class AddIndexesForPerformance extends Migration
      */
     public function up()
     {
-        Schema::table('episodes', function (Blueprint $table) {
-            $table->index('number');
-            $table->foreign('season_id')->references('id')->on('seasons');
-        });
-
-        Schema::table('seasons', function (Blueprint $table) {
-            $table->index('number');
-            $table->foreign('series_id')->references('id')->on('series');
-        });
-
         Schema::table('seen_episodes', function (Blueprint $table) {
             $table->foreign('episode_id')->references('id')->on('episodes');
             $table->foreign('user_id')->references('id')->on('users');
@@ -36,16 +26,6 @@ class AddIndexesForPerformance extends Migration
      */
     public function down()
     {
-        Schema::table('episodes', function (Blueprint $table) {
-            $table->dropIndex(['number']);
-            $table->dropForeign(['season_id']);
-        });
-
-        Schema::table('seasons', function (Blueprint $table) {
-            $table->dropIndex(['number']);
-            $table->dropForeign(['series_id']);
-        });
-
         Schema::table('seen_episodes', function (Blueprint $table) {
             $table->dropForeign(['episode_id', 'user_id']);
         });
