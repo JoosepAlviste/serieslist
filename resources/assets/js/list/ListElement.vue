6aba74132730dc10cdb509998ecef952a79f05f2
Joosep Alviste <joosep.alviste@gmail.com>
2017-11-14 20:10:26 +0100

    Fix small mistake

--- resources/assets/js/list/ListElement.vue
+++ resources/assets/js/list/ListElement.vue
@@ -64,7 +64,7 @@
              * @returns {String}
              */
             seriesLink() {
-                return `${this.series.id}`
+                return `/series/${this.series.id}`
             },
         },
 
