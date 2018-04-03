0ecc79ee86afaaaf66c14c1e1ab00387320e45d9
Joosep Alviste <joosep.alviste@gmail.com>
2018-04-03 22:01:40 +0200

    Refactor FileUploader a bit

--- app/FileUploader.php
+++ app/FileUploader.php
@@ -3,28 +3,46 @@
 namespace App;
 
 use Illuminate\Http\Request;
-use Intervention\Image\Facades\Image;
+use Intervention\Image\ImageManager;
 
 class FileUploader
 {
+    /** @var ImageManager */
+    private $image;
+
     /**
+     * FileUploader constructor.
+     *
+     * @param ImageManager $image
+     */
+    public function __construct(ImageManager $image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * Store the poster for a series from the request. Stores two versions of
+     * the image - a large one and a small one. Will return three paths, one
+     * for each file size and the standard filename without size postfix or file
+     * extension.
+     *
      * @param Request $request
      *
-     * @return array
+     * @return string[]
      */
     public function storeSeriesPoster($request)
     {
-        $file          = Image::make($request->file('poster'));
-        $path          = storage_path('app/public/posters') . '/';
-        $filenameHash  = md5($request->file('poster')->hashName() . microtime());
+        $file = $this->image->make($request->file('poster'));
+        $path = storage_path('app/public/posters') . '/';
+        $filenameHash = md5($request->file('poster')->hashName() . microtime());
         $largeFilename = $filenameHash . '-poster-large.png';
         $smallFilename = $filenameHash . '-poster-small.png';
 
         $file->encode('png')
-             ->resize(666, 1000)
-             ->save($path . $largeFilename)
-             ->resize(182, 268)
-             ->save($path . $smallFilename);
+            ->resize(666, 1000)
+            ->save($path . $largeFilename)
+            ->resize(182, 268)
+            ->save($path . $smallFilename);
 
         return [
             'largeFilename' => $largeFilename,
