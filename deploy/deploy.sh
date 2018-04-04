4b285d54d45c67a75131cd6b946c4f2414a7520a
Joosep Alviste <joosep.alviste@gmail.com>
2018-04-04 19:27:52 +0200

    Try to error in deploy process, see if Travis fails

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,11 +4,11 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
     read -r -d '' SSH_COMMAND << EOM
     cd /var/www/html/serieslist &&
     git pull &&
+    /not/a/normal/path &&
     composer install &&
-    php artisan migrate --force &&
-    php artisan db:seed --class=ProductionDatabaseSeeder
+    php artisan migrate --force
 EOM
-    ssh -o "StrictHostKeyChecking no" -i deploy/deploy-key deploy@46.101.147.166 $SSH_COMMAND
+    ssh -o "StrictHostKeyChecking no" -i deploy/deploy-key deploy@46.101.147.166 $SSH_COMMAND || return 1
 else
     echo "Not deploying, since this branch isn't master."
 fi
