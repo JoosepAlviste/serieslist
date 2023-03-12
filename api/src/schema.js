c454434400cb1318c34df5ff72cf2e0d869707d2
Joosep Alviste <joosep.alviste@gmail.com>
2023-03-12 19:12:51 +0200

    feat(api): set up a new GraphQL Yoga server
    Related to #27

---
+++ api/src/schema.js
@@ -0,0 +1,14 @@
+import { createSchema } from 'graphql-yoga'
+
+export const schema = createSchema({
+  typeDefs: /* GraphQL */ `
+  type Query {
+    hello: String
+  }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'world'
+    }
+  }
+})
