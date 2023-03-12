6a318d80144c35e6dae7cab73cab1cf6b7ca0ee9
Joosep Alviste <joosep.alviste@gmail.com>
2023-03-12 18:44:45 +0200

    feat: remove all old code
    Preparation for a new project.
    
    Related to #27

--- resources/views/series/partials/poster-thumbnail.blade.php
+++
@@ -1,7 +0,0 @@
-<figure class="image is-103x150">
-    @if ($series->poster)
-        <img src="{{ asset('storage/posters/' . $series->poster . '-poster-small.png') }}" alt="">
-    @else
-        <img src="{{ asset('images/placeholder_poster_small.png') }}" alt="">
-    @endif
-</figure>
--- resources/views/series/partials/series-list.blade.php
+++
@@ -1,5 +0,0 @@
-<ul class="series-list">
-    @foreach ($series as $oneSeries)
-        @include('series.partials.one-series', ['series' => $oneSeries])
-    @endforeach
-</ul>
