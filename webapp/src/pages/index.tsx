11c8ef4454f22f8783eb4effbc256d4f924e45f3
Joosep Alviste <joosep.alviste@gmail.com>
2023-03-15 08:23:24 +0200

    feat(webapp): set up new Next.js project
    Related to #27

---
+++ webapp/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Serieslist</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Serieslist</h1>
+      </main>
+    </>
+  )
+}
