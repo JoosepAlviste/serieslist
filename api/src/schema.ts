998998abd27f59c81381d9a641b6388d7fb9e2a1
Joosep Alviste <joosep.alviste@gmail.com>
2023-03-16 08:34:07 +0200

    build(api): set up Prettier
    Integrated with ESLint.
    
    Related to #27

--- api/src/schema.ts
+++ api/src/schema.ts
@@ -2,13 +2,13 @@ import { createSchema } from 'graphql-yoga'
 
 export const schema = createSchema({
   typeDefs: /* GraphQL */ `
-  type Query {
-    hello: String
-  }
+    type Query {
+      hello: String
+    }
   `,
   resolvers: {
     Query: {
-      hello: () => 'world'
-    }
-  }
+      hello: () => 'world',
+    },
+  },
 })
