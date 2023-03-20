46bc06263a28c11a771369f09259052ac1b75c9e
Joosep Alviste <joosep.alviste@gmail.com>
2023-03-20 20:27:49 +0200

    feat(api): create user table
    Related to #27

---
+++ api/src/migrations/2023-03-19T20:18:40-createUsersTable.ts
@@ -0,0 +1,24 @@
+import { sql, type Kysely } from 'kysely'
+
+import { type NotWorthIt } from '@/types/utils'
+
+export async function up(db: Kysely<NotWorthIt>): Promise<void> {
+  await db.schema
+    .createTable('user')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('name', 'varchar(255)', (col) => col.notNull())
+    .addColumn('email', 'varchar(255)', (col) => col.unique().notNull())
+    .addColumn('is_admin', 'boolean', (col) => col.defaultTo(false))
+    .addColumn('password', 'varchar(255)', (col) => col.notNull())
+    .addColumn('created_at', 'timestamptz', (col) =>
+      col.defaultTo(sql`NOW()`).notNull(),
+    )
+    .addColumn('updated_at', 'timestamptz', (col) =>
+      col.defaultTo(sql`NOW()`).notNull(),
+    )
+    .execute()
+}
+
+export async function down(db: Kysely<NotWorthIt>): Promise<void> {
+  await db.schema.dropTable('user').execute()
+}
