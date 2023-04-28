6ce5d8ceba9d6441b2bd405f8c0f688f44e67a91
Joosep Alviste <joosep.alviste@gmail.com>
2023-04-28 23:14:16 +0300

    feat(webapp): show authenticated user
    Related to #28

--- webapp/src/pages/about/index.page.tsx
+++ webapp/src/pages/about/index.page.tsx
@@ -1,13 +1,31 @@
+import { useQuery } from '@apollo/client'
 import React from 'react'
+
+import { graphql } from '@/generated/gql'
 import './code.css'
 
 export function Page() {
+  const { data } = useQuery(
+    graphql(`
+      query aboutPage {
+        me {
+          __typename
+          ... on User {
+            id
+            email
+          }
+        }
+      }
+    `),
+  )
+
   return (
     <>
       <h1>About</h1>
       <p>
         Example of using <code>vite-plugin-ssr</code>.
       </p>
+      <p>{JSON.stringify(data?.me)}</p>
     </>
   )
 }
