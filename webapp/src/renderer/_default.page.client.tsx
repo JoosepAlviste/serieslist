e7fa0a0a16730ffe501f7c1635caffe748261d38
Joosep Alviste <joosep.alviste@gmail.com>
2023-05-08 08:06:53 +0300

    fix(webapp): reuse Apollo instance on client-side page navigation
    Otherwise, the cache gets reset on every page navigation, meaning that
    the GraphQL queries are re-fetched.
    
    Related to #27

--- webapp/src/renderer/_default.page.client.tsx
+++ webapp/src/renderer/_default.page.client.tsx
@@ -1,3 +1,4 @@
+import { type NormalizedCacheObject, type ApolloClient } from '@apollo/client'
 import React from 'react'
 import { hydrateRoot, createRoot, type Root } from 'react-dom/client'
 
@@ -8,6 +9,11 @@ import { PageShell } from './PageShell'
 import type { PageContextClient } from './types'
 
 let root: Root | undefined
+/**
+ * Keep track of the Apollo client instance used in the client-side so that we
+ * don't create a new instance on every page load.
+ */
+let apollo: ApolloClient<NormalizedCacheObject> | undefined
 
 export function render(pageContext: PageContextClient) {
   const { Page, pageProps } = pageContext
@@ -19,9 +25,12 @@ export function render(pageContext: PageContextClient) {
     )
   }
 
-  pageContext.apollo = makeApolloClient({
-    initialState: pageContext.apolloInitialState,
-  })
+  // Create a new Apollo Client instance if it is not already created
+  apollo = pageContext.apollo =
+    apollo ??
+    makeApolloClient({
+      initialState: pageContext.apolloInitialState,
+    })
 
   const page = (
     <PageShell pageContext={pageContext}>
