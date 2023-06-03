a8daec51fdfcfc0a063fd06874161ec8ba16d817
Joosep Alviste <joosep.alviste@gmail.com>
2023-06-03 11:23:47 +0300

    feat(webapp): add tooltips to nav items

--- webapp/src/components/NavSidebar/NavSidebar.tsx
+++ webapp/src/components/NavSidebar/NavSidebar.tsx
@@ -1,7 +1,7 @@
 import * as NavigationMenu from '@radix-ui/react-navigation-menu'
 import React from 'react'
 
-import { Link } from '@/components'
+import { Link, Tooltip } from '@/components'
 
 import { Icon } from '../Icon'
 
@@ -10,19 +10,23 @@ import * as s from './NavSidebar.css'
 export const NavSidebar = () => (
   <NavigationMenu.Root orientation="vertical">
     <NavigationMenu.List className={s.container}>
-      <NavigationMenu.Item className={s.navItemLogo}>
-        <NavigationMenu.Link href="/" className={s.navLinkLogo}>
-          <Icon name="logo" size="l" label="Home" />
-        </NavigationMenu.Link>
-      </NavigationMenu.Item>
+      <Tooltip text="Home" side="right">
+        <NavigationMenu.Item className={s.navItemLogo}>
+          <NavigationMenu.Link href="/" className={s.navLinkLogo}>
+            <Icon name="logo" size="l" label="Home" />
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+      </Tooltip>
 
-      <NavigationMenu.Item>
-        <NavigationMenu.Link href="/about" className={s.navLink} asChild>
-          <Link activeClass={s.navLinkIsActive}>
-            <Icon name="series" size="l" label="Series" />
-          </Link>
-        </NavigationMenu.Link>
-      </NavigationMenu.Item>
+      <Tooltip text="My series list" side="right">
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="/about" className={s.navLink} asChild>
+            <Link activeClass={s.navLinkIsActive}>
+              <Icon name="series" size="l" label="Series" />
+            </Link>
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+      </Tooltip>
     </NavigationMenu.List>
   </NavigationMenu.Root>
 )
