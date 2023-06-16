f5987cba2eb4adff1ec57087950d81a82c27f780
Joosep Alviste <joosep.alviste@gmail.com>
2023-06-16 17:26:08 +0300

    feat(webapp): hide list page for unauthenticated users
    Related to #43

--- webapp/src/components/NavSidebar/NavSidebar.tsx
+++ webapp/src/components/NavSidebar/NavSidebar.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames'
 import React from 'react'
 
 import { Link, Tooltip } from '@/components'
+import { useAuthenticatedUser } from '@/features/auth'
 
 import { Icon } from '../Icon'
 
@@ -12,30 +13,39 @@ type NavSidebarProps = {
   className?: string
 }
 
-export const NavSidebar = ({ className }: NavSidebarProps) => (
-  <NavigationMenu.Root orientation="vertical" className={classNames(className)}>
-    <NavigationMenu.List className={s.container}>
-      <Tooltip text="Home" side="right">
-        <NavigationMenu.Item className={s.navItemLogo}>
-          <NavigationMenu.Link href="/" className={s.navLinkLogo}>
-            <Icon name="logo" size="l" label="Home" />
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-      </Tooltip>
+export const NavSidebar = ({ className }: NavSidebarProps) => {
+  const { currentUser } = useAuthenticatedUser()
 
-      <Tooltip text="My series list" side="right">
-        <NavigationMenu.Item>
-          <NavigationMenu.Link
-            href="/series/list"
-            className={s.navLink}
-            asChild
-          >
-            <Link activeClass={s.navLinkIsActive}>
-              <Icon name="series" size="l" label="Series" />
-            </Link>
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-      </Tooltip>
-    </NavigationMenu.List>
-  </NavigationMenu.Root>
-)
+  return (
+    <NavigationMenu.Root
+      orientation="vertical"
+      className={classNames(className)}
+    >
+      <NavigationMenu.List className={s.container}>
+        <Tooltip text="Home" side="right">
+          <NavigationMenu.Item className={s.navItemLogo}>
+            <NavigationMenu.Link href="/" className={s.navLinkLogo}>
+              <Icon name="logo" size="l" label="Home" />
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+        </Tooltip>
+
+        {currentUser && (
+          <Tooltip text="My series list" side="right">
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                href="/series/list"
+                className={s.navLink}
+                asChild
+              >
+                <Link activeClass={s.navLinkIsActive}>
+                  <Icon name="series" size="l" label="Series" />
+                </Link>
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+          </Tooltip>
+        )}
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
+  )
+}
