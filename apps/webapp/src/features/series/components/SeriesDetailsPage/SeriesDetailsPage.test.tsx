8478e6b07bfbf15b6b8976022f9fad7c788a2acf
Joosep Alviste <joosep.alviste@gmail.com>
2023-10-08 15:14:12 +0300

    style(webapp): import userEvent with named import
    Apparently, there is now a named export with the same name as the one
    that I used for the default import. So, ESLint warned that we should use
    the named import instead.

--- apps/webapp/src/features/series/components/SeriesDetailsPage/SeriesDetailsPage.test.tsx
+++ apps/webapp/src/features/series/components/SeriesDetailsPage/SeriesDetailsPage.test.tsx
@@ -1,5 +1,5 @@
 import { screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { userEvent } from '@testing-library/user-event'
 import { addDays, subDays } from 'date-fns'
 import React from 'react'
 
