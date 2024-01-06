14d1a27418b7bda1e850d6909746d020b994d658
Joosep Alviste <joosep.alviste@gmail.com>
2024-01-06 09:55:20 +0200

    ci: fail pipeline when deployment fails
    Previously, if the deployment got to the SSH step, then it would always
    pass because we were not handling errors from the SSH script.
    
    Related to #83

--- bin/update-deployment.sh
+++ bin/update-deployment.sh
@@ -6,14 +6,18 @@
 # To use this manually, fill the following variables in `.env`:
 # - DEPLOYMENT_SSH_HOST
 # - DEPLOYMENT_SSH_USER
-deploy() {
-  # If the required environment variables already exist (e.g., in GitHub
-  # Actions), then we don't need to read the `.env` file
-  if [[ -z "${DEPLOYMENT_SSH_HOST}" ]]; then
-    source .env
-  fi
 
-  ssh "${DEPLOYMENT_SSH_USER}@${DEPLOYMENT_SSH_HOST}" <<ENDSSH
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+COFF="\033[0m"
+
+# If the required environment variables already exist (e.g., in GitHub
+# Actions), then we don't need to read the `.env` file
+if [[ -z "${DEPLOYMENT_SSH_HOST}" ]]; then
+  source .env
+fi
+
+ssh "${DEPLOYMENT_SSH_USER}@${DEPLOYMENT_SSH_HOST}" <<ENDSSH
 cd ~/projects/serieslist
 git pull
 
@@ -25,7 +29,9 @@ echo "Migrating database..."
 docker compose -f docker-compose.production.yml exec api pnpm migrate:prod
 ENDSSH
 
-  echo "Project deployed!"
-}
-
-deploy
+if [[ $? = 0 ]]; then
+  printf "${GREEN}Deployment successful!${COFF}\n"
+else
+  printf "${RED}Deployment failed!${COFF}\n" >&2
+  exit 1
+fi
