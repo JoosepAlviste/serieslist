08adb692e5a113a51db8a932033972d95d23285c
Joosep Alviste <joosep.alviste@gmail.com>
2024-02-12 22:09:50 +0200

    feat(series): redirect logged in user from home to series list view
    This makes it much easier to use the application as a PWA. You just open
    the app and go directly to the list view. The home page isn't useful for
    logged in users anyways.
    
    Related to #70

---
+++ apps/webapp/src/pages/index/index.page.route.ts
@@ -0,0 +1,8 @@
+import { redirect } from 'vike/abort'
+import type { GuardSync } from 'vike/types'
+
+export const guard: GuardSync = (pageContext) => {
+  if (pageContext.currentUser) {
+    throw redirect('/series/list/in-progress')
+  }
+}
