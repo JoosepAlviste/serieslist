9c2e8e364eeb673c222e7ad02011f0efcc566533
Joosep Alviste <joosep.alviste@gmail.com>
2024-02-16 23:00:57 +0200

    feat(webapp): make PWA icons maskable
    No idea if this works, but let's see I suppose.
    
    https://web.dev/articles/maskable-icon
    
    Related to #70

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -59,16 +59,19 @@ export default ({ mode }: { mode: string }) => {
               src: 'pwa-64x64.png',
               sizes: '64x64',
               type: 'image/png',
+              purpose: 'any maskable',
             },
             {
               src: 'pwa-192x192.png',
               sizes: '192x192',
               type: 'image/png',
+              purpose: 'any maskable',
             },
             {
               src: 'pwa-512x512.png',
               sizes: '512x512',
               type: 'image/png',
+              purpose: 'any maskable',
             },
             {
               src: 'maskable-icon-512x512.png',
